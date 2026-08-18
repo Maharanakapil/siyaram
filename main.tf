980de6651f2d9198f6f70c9390102e3e1336bb3e
Kapil Ranai <kapilrana6592@gmail.com>
2026-08-19 00:37:27 +0530

    new RG2 added

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg" {
     name = "rg1"
     location = "central india"
 }
+
+resource "azurerm_resource_group" "rg" {
+    name = "rg2"
+    location = "south india"
+}
